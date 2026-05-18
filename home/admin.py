d8f1f865f5bf0d248e417c5e01362fdcd7f5ee31
assima08 <eago92@gmail.com>
2026-05-17 21:48:01 -0400

    registrations

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import   *
 
 # Register your models here.
+admin.site.register(Service)
+admin.site.register(Reservation)
+admin.site.register(Client)
+admin.site.register(Tarifs)
+admin.site.register(Expert)
+admin.site.register(ExpertService)
