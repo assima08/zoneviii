60bf81f10a45cc92368feacd5c85fb2a2e2e1fe5
assima08 <eago92@gmail.com>
2026-05-17 21:19:03 -0400

    migration vers postgresql

--- zoneviii/settings.py
+++ zoneviii/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'zoneviii.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'zoneviii',
+        'USER': 'postgres',
+        'PASSWORD': '228Degres@',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
