2821963b3be48cfd300641a65787340d028a6513
assima08 <eago92@gmail.com>
2026-05-18 01:15:23 -0400

    homepage frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,42 +1,12 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import ServiceCard from "./components/ServiceCard"
+import Navbar from "./components/Navbar"
 
 function App() {
 
-  const [services, setServices] = useState([])
-
-  useEffect(() => {
-
-    axios
-      .get("http://127.0.0.1:8000/services/")
-      .then((response) => {
-
-        setServices(response.data)
-
-      })
-      .catch((error) => {
-
-        console.log(error)
-
-      })
-
-  }, [])
-
   return (
-    <div>
 
-      <h1>ZoneVIII</h1>
+    <div className="app">
 
-      {services.map((service: any) => (
-
-        <ServiceCard
-          key={service.id}
-          nomService={service.nomService}
-          description={service.description}
-  />
-
-      ))}
+      <Navbar />
 
     </div>
   )
--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,45 +1,33 @@
-import { Menu } from "lucide-react"
+import "../styles/navbar.css"
 
 function Navbar() {
-  return (
-    <nav className="w-full fixed top-0 left-0 z-50 backdrop-blur-md bg-black/20 border-b border-white/10">
 
-      <div className="max-w-7xl mx-auto px-8 py-5 flex items-center justify-between">
+    return (
 
-        {/* Logo */}
-        <div className="text-3xl font-black tracking-tight text-white cursor-pointer">
-          ZoneVIII
-        </div>
+        <nav className="navbar">
 
-        {/* Navigation */}
-        <ul className="hidden md:flex items-center gap-12 text-sm uppercase tracking-[0.2em] text-white/80">
+            <div className="logo">
+                ZoneVIII
+            </div>
 
-          <li className="hover:text-blue-500 transition duration-300 cursor-pointer">
-            Services
-          </li>
+            <ul className="nav-links">
 
-          <li className="hover:text-blue-500 transition duration-300 cursor-pointer">
-            Tarifs
-          </li>
+                <li>Services</li>
 
-          <li className="hover:text-blue-500 transition duration-300 cursor-pointer">
-            Réservations
-          </li>
+                <li>Tarifs</li>
 
-          <li className="hover:text-blue-500 transition duration-300 cursor-pointer">
-            Nous contacter
-          </li>
+                <li>Réservations</li>
 
-        </ul>
+                <li>Nous contacter</li>
 
-        {/* Mobile menu icon */}
-        <button className="md:hidden text-white">
-          <Menu size={28} />
-        </button>
+            </ul>
 
-      </div>
-    </nav>
-  )
+            <div className="menu-icon">
+                ☰
+            </div>
+
+        </nav>
+    )
 }
 
 export default Navbar
